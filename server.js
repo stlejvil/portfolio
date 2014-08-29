db3190ea7ac437c67a98d5b548e77d75f79cd2a4
Stlejvil <shuncil_redart@hotmail.com>
2014-08-29 15:10:30 +0200

    Fixed 503 error

--- server.js
+++ server.js
@@ -7,8 +7,15 @@ var server = express();
 server.engine('jade', require('jade').__express);
 server.use(serveStatic('public'));
 
+server.ipaddress  = process.env.OPENSHIFT_NODEJS_IP;
+server.port       = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
 server.get('/', function(req, res) {
     res.render('index.jade');
 });
 
-server.listen(8080);
+// server.listen(8080);
+server.listen(server.port, server.ipaddress, function() {
+    console.log('%s: Node server started on %s:%d ...',
+        Date(Date.now() ), server.ipaddress, server.port);
+});
